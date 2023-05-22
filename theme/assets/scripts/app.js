05e869ea71b31308340bd56308b5ae3061886ecd
Vilmar Fonseca <vilmarfonsec@gmail.com>
2023-05-22 11:09:34 -0300

    Adding reusable components as a starting point for future projects

--- theme/assets/scripts/app.js
+++ theme/assets/scripts/app.js
@@ -10,3 +10,23 @@ function importAll(r) {
 importAll(require.context("../../blocks/", true, /\/script\.js$/))
 
 window.Alpine.start()
+
+// Carousel
+function changeCarouselVersionBasedOnScreen() {
+  if (globalWidth < 1280) {
+    document.querySelector('#carousel-mobile-1').checked = true;
+  }
+  if (globalWidth > 1280) {
+    document.querySelector('#carousel-1').checked = true;
+  }
+}
+
+let globalWidth = window.innerWidth
+let onresize = function (e) {
+  //note i need to pass the event as an argument to the function
+  globalWidth = e.target.outerWidth;
+  changeCarouselVersionBasedOnScreen()
+}
+
+changeCarouselVersionBasedOnScreen()
+window.addEventListener("resize", onresize);
